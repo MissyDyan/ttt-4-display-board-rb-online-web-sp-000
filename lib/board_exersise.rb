0fd7e85ad37feb8221ba0636a45a6c766f98f70e
Diane Phuong Nguyen <dianenguyen@gmail.com>
2021-06-12 04:39:09 +0000

    Automatically backed up by Learn

--- lib/board_exersise.rb
+++ lib/board_exersise.rb
@@ -4,6 +4,19 @@ board13 = [
   ["X", "O", "X"],
 ]
 
+def display_board(board)
+  i = 0
+  while i < 3
+    display_row(board[i])
+    puts '--------'
+    i++
+  end
+end
+
+def display_row(row)
+  
+end
+
 puts board13[1][1] # X
 puts board13[1][0] # O
 puts board13[2][0] # X
