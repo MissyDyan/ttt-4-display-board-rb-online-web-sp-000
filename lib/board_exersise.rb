00473642f3a5d39e5e722bf7cb8879ac5a43dd0f
Diane Phuong Nguyen <dianenguyen@gmail.com>
2021-06-12 04:42:47 +0000

    Automatically backed up by Learn

--- lib/board_exersise.rb
+++ lib/board_exersise.rb
@@ -6,11 +6,11 @@ board13 = [
 
 def display_board(board)
   i = 0
-  # while i < 3
+  while i < 3
     display_row(board[i])
     puts "-----------"
     i += 1
-  # end
+  end
 end
 
 def display_row(row)
