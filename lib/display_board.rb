2d19118249c51b78698f62b211608eb2cb7ba50b
Diane Phuong Nguyen <dianenguyen@gmail.com>
2021-06-12 04:33:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -82,14 +82,3 @@ display_board(board12)
 puts " "
 puts " "
 
-puts "---------"
-
-board13 = [
-  ["X", "O", "O"],
-  ["O", "X", "O"],
-  ["X", "O", "X"],
-]
-
-puts board13[1][1] # X
-puts board13[1][0] # O
-puts board13[2][0] # X
